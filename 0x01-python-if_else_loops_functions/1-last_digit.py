8084b1629c509056ce1c13c8703bdf08f5a0b7ce
danielchk <1487@holbertonschool.com>
2020-04-28 19:32:15 +0000

    Excercise 1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    res = (number % -10)
+else:
+    res = (number % 10)
+print("Last digist of", number, "is", res)
+
+if res == 0:
+    print("and is 0")
+elif res > 5:
+    print("and is greater than 5")
+else:
+    print("and is les than 6 and not 0")
