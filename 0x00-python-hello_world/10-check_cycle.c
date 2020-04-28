a4ed5a77446932762cd17030db554570fe1a56d9
danielchk <1487@holbertonschool.com>
2020-04-28 00:18:31 +0000

    Excercise 11

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * check_cycle - check if exist a list
+ * @list: head
+ * Return: 0 if it does not cycle, 1 if it does.
+ */
+
+int check_cycle(listint_t *list)
+{
+    
+}
