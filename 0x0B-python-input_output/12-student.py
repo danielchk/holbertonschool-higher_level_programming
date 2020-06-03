a48d0ca165a2f76b57512a541d56c4f9fdd7dde0
danielchk <hollowdaniel1@gmail.com>
2020-06-03 23:52:28 +0000

    excercise 12

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,24 @@
+"""
+module with class student
+"""
+
+
+class Student:
+    """student class"""
+    def __init__(self, first_name, last_name, age):
+        """student with 2 attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """representation of dictionaty of student class instance"""
+        if attrs is None:
+            return self.__dict__
+        dict2 = {}
+        for i in attrs:
+            try:
+                dict2[i] = self.__dict__[i]
+            except:
+                pass
+        return dict2
