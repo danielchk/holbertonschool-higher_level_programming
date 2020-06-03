8b4216870d9998e0fda0d40c2536239da0a2e19c
danielchk <hollowdaniel1@gmail.com>
2020-06-03 23:13:33 +0000

    fixing 5

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -6,5 +6,5 @@ import json
 
 
 def to_json_string(my_obj):
-	"""representation in json of the object"""
-	return json.dumps(my_obj)
+    """representation in json of the object"""
+    return json.dumps(my_obj)
