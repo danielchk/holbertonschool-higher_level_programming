f8db5afeb09f05ca67362c0a26ee918a6fcfb06b
danielchk <hollowdaniel1@gmail.com>
2020-06-03 23:04:01 +0000

    fix 4

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -6,4 +6,4 @@ modeule with function
 
 def append_write(filename="", text=""):
     with open(filename, mode="a", encoding="utf-8") as file:
-        return (file.write(str(text)))
+        return (file.write(text))
