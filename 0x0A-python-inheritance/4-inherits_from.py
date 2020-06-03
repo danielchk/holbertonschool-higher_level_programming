34313cff837e9ee41db43f1d5502f4c05b7b4a3e
danielchk <hollowdaniel1@gmail.com>
2020-06-03 00:01:46 +0000

    excercise 9 fix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,7 +5,6 @@ a class that inherited (directly or indirectly)
 from the specified class ; otherwise False
 """
 
-
 def inherits_from(obj, a_class):
     """
     Returns true if the object is an instance of a class that inherited
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-module with class 
+module with class
 """
 
 
