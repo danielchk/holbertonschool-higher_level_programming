bf77478c837152a8cfb5f69df43ee3f0476fe6e6
danielchk <hollowdaniel1@gmail.com>
2020-12-13 14:02:04 -0500

    but why?

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-#lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
-
-import MySQLdb
+# Lists all states with a name starting with N from the database hbtn_0e_0_usa.
+# Usage: ./1-filter_states.py <mysql username> \
+#                             <mysql password> \
+#                             <database name>
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    nav = db.cursor()
-    nav.execute("SELECT * FROM `states` ORDER BY `id`")
-    [print(state) for state in nav.fetchall() if state[1][0] == "N"]
+    c = db.cursor()
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
