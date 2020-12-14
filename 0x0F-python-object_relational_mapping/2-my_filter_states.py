c691580922287b10063cd6eb4719587e95fdb052
danielchk <hollowdaniel1@gmail.com>
2020-12-14 17:04:24 -0500

    pep8

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-takes in an argument and displays all values in the states 
+takes in an argument and displays all values in the states
 table of where name matches the argument.
 """
 
