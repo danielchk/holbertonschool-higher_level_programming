eed440b2fc9553a927dc1a873d0a59c34404d85f
danielchk <hollowdaniel1@gmail.com>
2020-12-17 18:37:11 -0500

    permissions in 12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-
+script that changes the name of a State object from the database hbtn_0e_6_usa
 """
 import sys
 from sqlalchemy import create_engine
@@ -9,7 +9,7 @@ from model_state import State
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+                           .format(argv[1], argv[2], argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
 
