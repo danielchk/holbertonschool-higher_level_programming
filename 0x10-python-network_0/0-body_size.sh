76df7619f27ba91fa121df1265a5c84cd9319a24
danielchk <hollowdaniel1@gmail.com>
2020-12-18 12:17:31 -0500

    Error first line

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/python3
-"""script that takes in a URL, sends a request to that URL,and displays the size of the body of the response"""
-curl -s "$1" | wc -c
+#!/bin/bash
+#displays the size of the body of the response"""
+curl -s " $1" | wc -c
