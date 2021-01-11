c2727b2d724fd4589a283d5a8c7634c78d841fc4
danielchk <hollowdaniel1@gmail.com>
2021-01-11 16:25:14 -0500

    excercise 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,17 +2,13 @@
 """script that takes in a URL, sends a request
 to the URL and displays the body of the response"""
 
-import urllib.request
-import urllib.parse
+import urllib.error as error
+import urllib.request as request
 from sys import argv
 if __name__ == "__main__":
-    """
-    """
-    info = {'email': argv[2]}
-    data = urllib.parse.urlencode(info)
-    data = data.encode('ascii')
-    url = argv[1]
-    reply = urllib.request.Request(url, data)
-    with urllib.request.urlopen(reply) as rep:
-        body = rep.read()
-    print(body.decode(encoding="utf-8"))
+    req = request.Request(argv[1])
+    try:
+        with request.urlopen(req) as r:
+            print(r.read().decode('utf-8'))
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
