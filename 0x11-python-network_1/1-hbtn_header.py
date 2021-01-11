d8dc020ced5e201cdb8e1809b9a8228ac8d3672d
danielchk <hollowdaniel1@gmail.com>
2021-01-11 15:01:40 -0500

    fixing 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import sys
-import urllib.request
 """script that takes in a URL, sends a request to
 the URL and displays the value
 of the X-Request-Id variable found in the header of the response."""
 
+import urllib.request as request
+from sys import argv
 if __name__ == "__main__":
     req = request.Request(argv[1])
     with request.urlopen(req) as r:
