961d5c259695cba9752c178c195a891bd35f52b5
danielchk <hollowdaniel1@gmail.com>
2021-01-11 16:56:37 -0500

    fixing 6 and 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""url requests with error handling"""
+
+import requests
+from sys import argv
+if __name__ == "__main__":
+    req = requests.get(argv[1])
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
