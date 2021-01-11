78e763db7dd7a5b54de3797808a7a035496ea08c
danielchk <hollowdaniel1@gmail.com>
2021-01-11 14:56:03 -0500

    pep8

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import sys
 import urllib.request
-"""script that takes in a URL, sends a request to the URL and displays the value 
+"""script that takes in a URL, sends a request to
+the URL and displays the value
 of the X-Request-Id variable found in the header of the response."""
 
 if __name__ == "__main__":
