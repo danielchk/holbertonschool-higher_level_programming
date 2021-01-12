c80a0d0e74713190b26b12e535f97137d4513005
danielchk <hollowdaniel1@gmail.com>
2021-01-12 09:28:31 -0500

    excercise 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""
-takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
-with the letter as a parameter
-"""
+"""script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter."""
+
+import requests
+from sys import argv
 if __name__ == '__main__':
-    import requests
-    from sys import argv
     if len(argv) == 2:
-        q = argv[1]
+        req = argv[1]
     else:
-        q = ""
-    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+        req = ""
+    postr = requests.post('http://0.0.0.0:5000/search_user', data={'req': req})
     try:
-        r_dict = r.json()
-        id = r_dict.get('id')
-        name = r_dict.get('name')
-        if len(r_dict) == 0 or not id or not name:
+        jsondic = postr.json()
+        id = jsondic.get('id')
+        name = jsondic.get('name')
+        if len(jsondic) == 0 or not id or not name:
             print("No result")
         else:
-            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
+            print("[{}] {}".format(jsondic.get('id'), jsondic.get('name')))
     except:
         print("Not a valid JSON")
